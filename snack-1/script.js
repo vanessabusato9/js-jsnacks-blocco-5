fa0fa0323e60e1f606170d0b710105067dd31496
vanessa <vane.busato9@gmail.com>
2025-11-27 13:18:49 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for ( let i = 0; i <= names.length; i++) {
+    console.log(names);
+}
