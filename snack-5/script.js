f3656069bfde1b34591c324e25bc9f5b2460a5d1
vanessa <vane.busato9@gmail.com>
2025-12-01 19:37:01 +0100

    es.5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const numeriPari = nums.filter(num => num % 2 === 0);
+
+console.log(numeriPari);
