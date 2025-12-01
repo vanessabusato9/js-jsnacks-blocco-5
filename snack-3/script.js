2c1de22a736931ce087af3577d3506978c8caf5e
vanessa <vane.busato9@gmail.com>
2025-12-01 19:23:44 +0100

    es.3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const incrementati = numbers.map(num => num + 1);
+
+console.log(incrementati);
