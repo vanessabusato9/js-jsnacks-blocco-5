d64b2053a8ffca8e6cabab489d92f00e7dac5cf6
vanessa <vane.busato9@gmail.com>
2025-12-02 13:41:41 +0100

    es.8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classe = students.find(student => student.name === 'Marco Lanci').class;
+
+console.log(classe);
