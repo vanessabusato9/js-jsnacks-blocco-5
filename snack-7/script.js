1e166e098cb21ef16f101bb8823faafbf9ea8d02
vanessa <vane.busato9@gmail.com>
2025-12-02 13:40:35 +0100

    es.7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+const student = students.find(stu => stu.id === 2);
+
+console.log(student);
